bc6495a5c78701063480391d146185a90940f956
muham <ayvazemir1@gmail.com>
2025-11-30 18:21:38 +0300

    Filmler uygulamasi bloc pattern

--- lib/uı/views/anasayfa.dart
+++ lib/uı/views/anasayfa.dart
@@ -1,6 +1,8 @@
 import 'package:filmler_app/data/entity/filmler.dart';
+import 'package:filmler_app/u%C4%B1/cubit/anasayfa_cubit.dart';
 import 'package:filmler_app/u%C4%B1/views/detay_sayfa.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -10,23 +12,12 @@ class Anasayfa extends StatefulWidget {
 }
 
 class _AnasayfaState extends State<Anasayfa> {
-  Future<List<Filmler>> filmleriYukle() async {
-    var filmlerListesi = <Filmler>[];
-    var f1 = Filmler(id: 1, ad: "Django", resim: "django.png", fiyat: 24);
-    var f2 = Filmler(id: 2, ad: "Insterstellar", resim: "interstellar.png", fiyat: 32);
-    var f3 = Filmler(id: 3, ad: "Inception", resim: "inception.png", fiyat: 24);
-    var f4 = Filmler(id: 4, ad: "The Hateful Eight", resim: "thehatefuleight.png", fiyat: 24);
-    var f5 = Filmler(id: 5, ad: "The Pianist", resim: "thepianist.png", fiyat: 24);
-    var f6 = Filmler(id: 6, ad: "Anadoluda", resim: "anadoluda.png", fiyat: 24);
-    filmlerListesi.add(f1);
-    filmlerListesi.add(f2);
-    filmlerListesi.add(f3);
-    filmlerListesi.add(f4);
-    filmlerListesi.add(f5);
-    filmlerListesi.add(f6);
-    return filmlerListesi;
-    
 
+  @override
+  void initState() {
+
+    super.initState();
+    context.read<AnasayfaCubit>().filmleriYukle();
   }
   @override
   Widget build(BuildContext context) {
@@ -36,18 +27,18 @@ class _AnasayfaState extends State<Anasayfa> {
         title: const Text("Filmler"),
 
       ),
-      body: FutureBuilder<List<Filmler>>(
-        future: filmleriYukle(),
-        builder: (context,snapshot){
-          if(snapshot.hasData){
-            var filmerListesi = snapshot.data;
+      body: BlocBuilder<AnasayfaCubit,List<Filmler>>(
+
+        builder: (context,filmlerListesi){
+          if(filmlerListesi.isNotEmpty){
+
             return GridView.builder(
-              itemCount: filmerListesi!.length,
+              itemCount: filmlerListesi.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,childAspectRatio: 1 / 1.8,
               ),
               itemBuilder: (context,indeks){
-                var film = filmerListesi[indeks];
+                var film = filmlerListesi[indeks];
                 return GestureDetector(
                   onTap: (){
                     Navigator.push(context, MaterialPageRoute(builder: (context)=> DetaySayfa(film: film)));
